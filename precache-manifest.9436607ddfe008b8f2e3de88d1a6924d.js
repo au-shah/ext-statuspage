1765cfed835916167b02d6b4bfa65b0ea0ea4d0e
Akshat Shah <akshatshah@akshats-mbp.hursley.uk.ibm.com>
2020-03-02 15:51:05 +0000

    Updates

---
+++ precache-manifest.9436607ddfe008b8f2e3de88d1a6924d.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b08fca61744c738613f7fbd30d1c949f",
+    "url": "/ext-statuspage/index.html"
+  },
+  {
+    "revision": "b3ac7ffbc4f75e086cf3",
+    "url": "/ext-statuspage/static/css/2.9702ecfc.chunk.css"
+  },
+  {
+    "revision": "a0cca582f7f4772c04c5",
+    "url": "/ext-statuspage/static/css/main.dbb7bda4.chunk.css"
+  },
+  {
+    "revision": "b3ac7ffbc4f75e086cf3",
+    "url": "/ext-statuspage/static/js/2.7082d83a.chunk.js"
+  },
+  {
+    "revision": "35e9035c4541049d41232fe0bf96f70b",
+    "url": "/ext-statuspage/static/js/2.7082d83a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a0cca582f7f4772c04c5",
+    "url": "/ext-statuspage/static/js/main.b3f3a291.chunk.js"
+  },
+  {
+    "revision": "3573eb180c1c184ae180",
+    "url": "/ext-statuspage/static/js/runtime-main.5a80f917.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/ext-statuspage/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/ext-statuspage/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/ext-statuspage/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/ext-statuspage/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/ext-statuspage/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "ec17a1ea0f5297f8e2c40893796fbffd",
+    "url": "/ext-statuspage/static/media/icon_light.ec17a1ea.svg"
+  }
+]);
